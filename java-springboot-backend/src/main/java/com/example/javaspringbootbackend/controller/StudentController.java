5b1b80b18a1b1b8ed726f7bf2643f4d189ce7699
JiaoyangXu <59169603+JiaoyangXu@users.noreply.github.com>
2022-06-26 11:20:59 +0800

    add create student restful api -- backend

--- java-springboot-backend/src/main/java/com/example/javaspringbootbackend/controller/StudentController.java
+++ java-springboot-backend/src/main/java/com/example/javaspringbootbackend/controller/StudentController.java
@@ -3,10 +3,7 @@ package com.example.javaspringbootbackend.controller;
 import com.example.javaspringbootbackend.model.Student;
 import com.example.javaspringbootbackend.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,11 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getAllStudents () {
         return studentRepository.findAll();
+    }
+
+    // create student rest api
+    @PostMapping("/students")
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
     }
 }
